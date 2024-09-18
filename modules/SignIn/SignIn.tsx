f504c60f69186838fca848a435650b753e2f70db
Mykola Kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2024-09-18 17:23:42 +0300

    feat(log-in): added navigate to home page on login

--- modules/SignIn/SignIn.tsx
+++ modules/SignIn/SignIn.tsx
@@ -1,13 +1,13 @@
 import { memo, useRef } from "react";
 import { TextInput, View } from "react-native";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { useSignIn } from "@/hooks/api/useAuthApi";
 import { useSignInForm } from "./hooks/useSignInForm";
+import { toast } from "@/utils/toast";
 import { InputField } from "@/components/InputField";
 import Typography from "@/components/Typography";
 import Button from "@/components/Button";
 import s from "./SignIn.styles";
-import { useSignIn } from "@/hooks/api/useAuthApi";
-import { toast } from "@/utils/toast";
 
 const SignIn: React.FC = () => {
   const passwordFieldRef = useRef<TextInput>(null);
@@ -32,6 +32,8 @@ const SignIn: React.FC = () => {
         onSuccess() {
           // TODO: navigate to the main screen
           toast("Logged in successfully", { variant: "success" });
+
+          router.replace("/(tabs)/home");
         },
         onError(error) {
           toast(error.message, { variant: "error" });
