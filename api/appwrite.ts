dd1079539d5b4afbbb4fe3976ff0207887b745a2
Mykola Kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2024-09-18 14:43:17 +0300

    feat(env): adde env variabled config. updated apperoite config

--- api/appwrite.ts
+++ api/appwrite.ts
@@ -1,5 +1,9 @@
 import { Client } from "react-native-appwrite";
+import { APPWRITE_ENDPOINT, APPWRITE_PLATFORM, APPWRITE_PROJECT } from "@env";
 
-const client = new Client();
+const client = new Client()
+  .setEndpoint(APPWRITE_ENDPOINT)
+  .setProject(APPWRITE_PROJECT)
+  .setPlatform(APPWRITE_PLATFORM);
 
 export default client;
